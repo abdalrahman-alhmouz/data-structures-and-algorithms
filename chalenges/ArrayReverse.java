37108bdb9492bf6d831e5f61399f05fff5f7026f
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2021-04-04 21:59:16 +0300

    init comit

---
+++ chalenges/ArrayReverse.java
@@ -0,0 +1,24 @@
+  import java.util.Arrays;
+
+class Main {
+  public static void main(String[] args) {
+int[] i2 = {1, 2, 3,4,5,6};
+    System.out.println(Arrays.toString(reverseArray(i2)));
+    System.out.println("Hello world!");
+
+  }
+
+
+    static int[] reverseArray (int[] array){
+      int[] arrayOne =new int[array.length] ;
+        for(int i=0;i<array.length ;i++){
+            // Scanner input =new Scanner(System.in) ;
+            arrayOne[i]=array[array.length-1-i];
+        }
+                return arrayOne ;
+
+    }
+
+
+}
+
