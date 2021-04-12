c3d5396b335190f3696feeb084b94d03fc4196df
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2021-04-12 20:56:33 +0300

    init comit

--- chalenges/linked list/linkedListTest.java
+++ chalenges/linked list/linkedListTest.java
@@ -54,5 +54,27 @@ public class AppTest {
 
         assertEquals("head->[5]->[6]->[7]->[8]->x",classUnderTest.insertAfter(7,8));
     }
+    // _____________________________________________{{{{code Chaleng 07}}}}
+      @Test public void returnValueTest() {
+        linkeList classUnderTest = new linkeList();
+        classUnderTest.append(5);
+        classUnderTest.append(6);
+        classUnderTest.append(7);
+        classUnderTest.append(9);
+        classUnderTest.append(10);
+
+
+        assertEquals("Exeption",classUnderTest.returnValue(5));
+        assertEquals("5",classUnderTest.returnValue(4));
+        assertEquals("Exeption",classUnderTest.returnValue(-5));
+        assertEquals("7",classUnderTest.returnValue(2));
+    }
+    @Test public void returnValueTestTow() {
+        linkeList classUnderTest = new linkeList();
+//        classUnderTest.append(5);
+        classUnderTest.append(7);
+        assertEquals("7",classUnderTest.returnValue(0));
+    }
+    // _____________________________________________{{{{code Chaleng 07}}}}
 
 }
